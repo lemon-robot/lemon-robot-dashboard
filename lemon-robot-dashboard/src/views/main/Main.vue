ed5e2ef110c7821f8aa7c517b1280e71eba4e079
LemonITCN <liuri@lemonit.cn>
2019-07-22 16:43:23 +0800

    添加codestyle.xml

--- lemon-robot-dashboard/src/views/main/Main.vue
+++ lemon-robot-dashboard/src/views/main/Main.vue
@@ -6,29 +6,29 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import Component from 'vue-class-component'
-  import ControlMenu from './control-menu/ControlMenu.vue'
-  import LoginService from '@/service/login/LoginService'
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import ControlMenu from './control-menu/ControlMenu.vue'
+import LoginService from '@/service/login/LoginService'
 
-  @Component({
-    components: {
-      ControlMenu
-    }
-  })
-  export default class Main extends Vue {
-    lang = 'main.'
+@Component({
+  components: {
+    ControlMenu
+  }
+})
+export default class Main extends Vue {
+  lang = 'main.'
 
-    created () {
-      if (!LoginService.getLoginState()) {
-        this.$notify.warning({
-          title: this.$t(this.lang + 'not_login_tip_title').toString(),
-          message: this.$t(this.lang + 'not_login_tip_message').toString()
-        })
-        this.$router.replace('/login')
-      }
+  created() {
+    if (!LoginService.getLoginState()) {
+      this.$notify.warning({
+        title: this.$t(this.lang + 'not_login_tip_title').toString(),
+        message: this.$t(this.lang + 'not_login_tip_message').toString()
+      })
+      this.$router.replace('/login')
     }
   }
+}
 </script>
 
 <style scoped lang="scss">
