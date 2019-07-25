106e3bb12c5c8a42c1ecb430b7281fc772218d7e
sunjunhe <499425242@qq.com>
2019-07-25 13:48:07 +0800

    import environment-manager

--- lemon-robot-dashboard/src/lang/en/main/functions/index.ts
+++ lemon-robot-dashboard/src/lang/en/main/functions/index.ts
@@ -1,7 +1,10 @@
 import dispatcherManager from './dispatcher-manager'
+import environmentManager from './environment-manager'
 import systemSettings from './system-settings'
 
 export default {
   dispatcher_manager: dispatcherManager,
+  environment_manager: environmentManager,
   system_settings: systemSettings
 }
+
